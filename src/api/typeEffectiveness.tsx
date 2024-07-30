aea3f3669446872c108722083022486124817bd0
lazipops <78057304+lazipops@users.noreply.github.com>
2024-07-30 15:10:45 -0400

    styled type effectiveness table + altered typeEffectiveness object to included *1 damaged types
    logic is not sound for pokemon with dual typing yet.

--- src/api/typeEffectiveness.tsx
+++ src/api/typeEffectiveness.tsx
@@ -1,89 +1,111 @@
 const typeEffectiveness = {
-  normal: { weak: ["fighting"], resist: [], immune: ["ghost"] },
+  normal: {
+    weak: ['fighting'],
+    resist: [],
+    immune: ['ghost'],
+    normal: ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'dragon', 'dark', 'steel', 'fairy']
+  },
   fire: {
-    weak: ["water", "rock", "ground"],
-    resist: ["fire", "grass", "ice", "bug", "steel", "fairy"],
+    weak: ['water', 'rock', 'ground'],
+    resist: ['fire', 'grass', 'ice', 'bug', 'steel', 'fairy'],
     immune: [],
+    normal: ['normal', 'electric', 'fighting', 'poison', 'flying', 'psychic', 'ghost', 'dragon', 'dark']
   },
   water: {
-    weak: ["electric", "grass"],
-    resist: ["fire", "water", "ice", "steel"],
+    weak: ['electric', 'grass'],
+    resist: ['fire', 'water', 'ice', 'steel'],
     immune: [],
+    normal: ['normal', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'fairy']
   },
   electric: {
-    weak: ["ground"],
-    resist: ["electric", "flying", "steel"],
+    weak: ['ground'],
+    resist: ['electric', 'flying', 'steel'],
     immune: [],
+    normal: ['normal', 'fire', 'water', 'grass', 'ice', 'fighting', 'poison', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'fairy']
   },
   grass: {
-    weak: ["fire", "ice", "poison", "flying", "bug"],
-    resist: ["water", "electric", "grass", "ground"],
+    weak: ['fire', 'ice', 'poison', 'flying', 'bug'],
+    resist: ['water', 'electric', 'grass', 'ground'],
     immune: [],
-  },
-  fighting: {
-    weak: ["flying", "psychic", "fairy"],
-    resist: ["rock", "bug", "dark"],
-    immune: [],
-  },
-  flying: {
-    weak: ["rock", "electric", "ice"],
-    resist: ["fighting", "bug", "grass"],
-    immune: [],
-  },
-  poison: {
-    weak: ["ground", "psychic"],
-    resist: ["fighting", "bug", "poison", "grass", "fairy"],
-    immune: [],
-  },
-  ground: {
-    weak: ["water", "grass", "ice"],
-    resist: ["rock", "poison"],
-    immune: ["electric"],
-  },
-  rock: {
-    weak: ["fighting", "ground", "steel", "water", "grass"],
-    resist: ["normal", "flying", "poison", "fire"],
-    immune: [],
-  },
-  bug: {
-    weak: ["flying", "rock", "fire"],
-    resist: ["fighting", "ground", "grass"],
-    immune: [],
-  },
-  ghost: {
-    weak: ["ghost", "dark"],
-    resist: ["poison", "bug"],
-    immune: ["normal", "fighting"],
-  },
-  steel: {
-    weak: ["fighting", "ground", "fire"],
-    resist: ["normal", "bug", "flying", "rock", "steel", "grass", "psychic", "ice", "dragon", "fairy"],
-    immune: ["poison"],
-  },
-  psychic: {
-    weak: ["bug", "ghost", "dark"],
-    resist: ["fighting", "psychic"],
-    immune: [],
+    normal: ['normal', 'fighting', 'psychic', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
   },
   ice: {
-    weak: ["fighting", "rock", "steel", "fire"],
-    resist: ["ice"],
+    weak: ['fire', 'fighting', 'rock', 'steel'],
+    resist: ['ice'],
     immune: [],
+    normal: ['normal', 'water', 'electric', 'grass', 'poison', 'ground', 'flying', 'psychic', 'bug', 'ghost', 'dragon', 'dark', 'fairy']
+  },
+  fighting: {
+    weak: ['flying', 'psychic', 'fairy'],
+    resist: ['bug', 'rock', 'dark'],
+    immune: [],
+    normal: ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'poison', 'ground', 'ghost', 'dragon', 'steel']
+  },
+  poison: {
+    weak: ['ground', 'psychic'],
+    resist: ['grass', 'fighting', 'poison', 'bug', 'fairy'],
+    immune: [],
+    normal: ['normal', 'fire', 'water', 'electric', 'ice', 'flying', 'psychic', 'rock', 'ghost', 'dragon', 'dark', 'steel']
+  },
+  ground: {
+    weak: ['water', 'ice', 'grass'],
+    resist: ['poison', 'rock'],
+    immune: ['electric'],
+    normal: ['normal', 'fire', 'fighting', 'ground', 'flying', 'psychic', 'bug', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+  },
+  flying: {
+    weak: ['electric', 'ice', 'rock'],
+    resist: ['grass', 'fighting', 'bug'],
+    immune: ['ground'],
+    normal: ['normal', 'fire', 'water', 'electric', 'poison', 'flying', 'psychic', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+  },
+  psychic: {
+    weak: ['bug', 'ghost', 'dark'],
+    resist: ['fighting', 'psychic'],
+    immune: [],
+    normal: ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'poison', 'ground', 'flying', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+  },
+  bug: {
+    weak: ['fire', 'flying', 'rock'],
+    resist: ['grass', 'fighting', 'ground'],
+    immune: [],
+    normal: ['normal', 'water', 'electric', 'ice', 'poison', 'bug', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
+  },
+  rock: {
+    weak: ['water', 'grass', 'fighting', 'ground', 'steel'],
+    resist: ['normal', 'fire', 'poison', 'flying'],
+    immune: [],
+    normal: ['electric', 'ice', 'psychic', 'bug', 'ghost', 'dragon', 'dark', 'fairy']
+  },
+  ghost: {
+    weak: ['ghost', 'dark'],
+    resist: ['poison', 'bug'],
+    immune: ['normal', 'fighting'],
+    normal: ['fire', 'water', 'electric', 'grass', 'ice', 'ground', 'flying', 'psychic', 'rock', 'dragon', 'steel', 'fairy']
   },
   dragon: {
-    weak: ["ice", "dragon", "fairy"],
-    resist: ["fire", "water", "grass", "electric"],
+    weak: ['ice', 'dragon', 'fairy'],
+    resist: ['fire', 'water', 'electric', 'grass'],
     immune: [],
+    normal: ['normal', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dark', 'steel', 'fairy']
   },
   dark: {
-    weak: ["fighting", "bug", "fairy"],
-    resist: ["ghost", "dark"],
-    immune: ["psychic"],
+    weak: ['fighting', 'bug', 'fairy'],
+    resist: ['ghost', 'dark'],
+    immune: ['psychic'],
+    normal: ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'poison', 'ground', 'flying', 'rock', 'dragon', 'steel', 'fairy']
+  },
+  steel: {
+    weak: ['fire', 'fighting', 'ground'],
+    resist: ['normal', 'grass', 'ice', 'flying', 'psychic', 'bug', 'rock', 'dragon', 'steel', 'fairy'],
+    immune: ['poison'],
+    normal: ['water', 'electric', 'ghost', 'dark']
   },
   fairy: {
-    weak: ["poison", "steel"],
-    resist: ["fighting", "bug", "dark"],
-    immune: ["dragon"],
+    weak: ['poison', 'steel'],
+    resist: ['fighting', 'bug', 'dark'],
+    immune: ['dragon'],
+    normal: ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'ground', 'flying', 'psychic', 'rock', 'ghost', 'dragon', 'steel']
   }
 };
 
