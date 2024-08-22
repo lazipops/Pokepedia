0e014d92d9b0b1bdcba482e47f276003a9c935a7
lazipops <78057304+lazipops@users.noreply.github.com>
2024-08-22 16:00:53 -0400

    changed HashRouter to BrowserRouter since github pages does not support HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import {
   Navigate,
-  HashRouter as Router,
+  BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
