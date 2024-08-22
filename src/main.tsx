abf085022a709f83a752dddcd79241cf6a2f6b9f
lazipops <78057304+lazipops@users.noreply.github.com>
2024-08-22 15:02:51 -0400

    make sure that App.tsx is correctly transpiled to a JS file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
