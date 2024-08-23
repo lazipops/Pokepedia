b8c591a201fe478fa87911467615c6a9e794cb62
lazipops <78057304+lazipops@users.noreply.github.com>
2024-08-23 14:34:22 -0400

    github pages stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000 //use port 3000 instead of the default
