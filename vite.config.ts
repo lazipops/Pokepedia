7635c9a1f57aac6e3b38d49a118200dd68533fd6
lazipops <78057304+lazipops@users.noreply.github.com>
2024-08-23 14:52:30 -0400

    vite config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/Pokepedia/",
   plugins: [react()],
   server: {
     port: 3000 //use port 3000 instead of the default
